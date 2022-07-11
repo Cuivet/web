92c5e7481d30b673bc06dac213a1bbc6920df7c0
FrattinJuan <frattin.juan9@gmail.com>
2022-07-11 11:06:12 -0300

    fix: validaciones del registro de usuario

--- src/components/Admin/RegisterForm/RegisterForm.js
+++ src/components/Admin/RegisterForm/RegisterForm.js
@@ -1,13 +1,14 @@
 import React, {useState} from "react";
-import {Form, Input, Button, Checkbox, Select, notification} from 'antd'
+import {Form, Input, Button, Checkbox, Select, notification} from 'antd';
+import {emailValidation, minLengthValidation} from '../../../utils/formValidation'
 
 import './RegisterForm.scss'
 import { LoadingOutlined, LockOutlined, UserOutlined, MailOutlined, PhoneOutlined, IdcardOutlined} from "@ant-design/icons";
 
 
-
 //componente formulario del LogIn para nuevos usuarios
 export default function RegisterForm(){
+    //donde almacena los datos del formulario
     const [input, setInput]= useState({
         email:"",
         password:"",
@@ -22,6 +23,21 @@ export default function RegisterForm(){
 
     });
 
+    //validaciones en el formulario
+    const [formValid, setFormValid] = useState({
+        email: false,
+        password: false,
+        repeatPassword: false,
+        name: false,
+        lastName: false,
+        dni:false,
+        phone:false,
+        profile: false,
+        mp: false,
+        privacyPolicy:false
+    });
+
+    //setea los valores del form a la variable input
     const changeForm = e =>{
         console.log(e.target.name);
         if(e.target.name === "privacyPolicy"){
@@ -38,8 +54,70 @@ export default function RegisterForm(){
         }
     };
 
+    //validacion de datos en el formulario
+    const inputValidation = e =>{
+        
+        const {type, name} = e.target;
+
+        if(type === "email"){
+            setFormValid({
+                ...formValid,
+                [name]:emailValidation(e.target)
+            });
+        };
+        if(type ==="password"){
+            setFormValid({
+                ...formValid,
+                [name]:minLengthValidation(e.target, 8)
+            });
+        };
+        if(type === "text"){
+            setFormValid({ ...formValid, [name]:minLengthValidation(e.target, 2)});
+        };
+        // if(type === "number"){
+        //     setFormValid({ phone:true, dni:true, mp:true})
+        // };
+        if(type ==="checkbox"){
+            setFormValid({
+                ...formValid,
+                [name]:e.target.checked
+            });
+        };
+    }
+
     const register = e => {
-        console.log(input)
+        console.log(input);
+        console.log(formValid);
+        const {email, password, repeatPassword, name, lastName, phone, dni, privacyPolicy, profile} = formValid;
+
+        const emailVal = input.email;
+        const passwordVal = input.password;
+        const repeatPasswordVal = input.repeatPassword;
+        const nameVal = input.name;
+        const lastNameVal = input.lastName;
+        const dniVal = input.dni;
+        const phoneVal = input.phone;
+        const profileVal = input.profile;
+        const privacyPolicyVal = input.privacyPolicy;
+
+        if(!emailVal || !passwordVal ||!repeatPasswordVal|| !nameVal|| !lastNameVal|| !phoneVal|| !dniVal|| !profileVal|| !privacyPolicyVal){
+            notification['error']({
+                message: "Todos los campos son obligatorios",
+                description: "Debe completar todos los campos para poder registrarse",
+                placement: "top"
+            })
+        } else{
+            if(password !== repeatPasswordVal){
+                notification['error']({
+                    message: "Las constrasenias deben ser iguales",
+                    description: "Compruebe que las contrasenias ingresadas coincidan",
+                    placement: "top"
+                })
+            } else{
+                
+            }
+            
+        }
     };
 
     //para mostrar/ocultar campo de matricula segun perfil 
@@ -48,34 +126,47 @@ export default function RegisterForm(){
     
     const onProfileChange = (val) => {
         console.log(vet);
-        if (val === '2'){
-            setVet(true);
-            setInput({profile: "2"});
+       
+        switch(val){
+            case '1':
+                setVet(false);
+                setInput({profile:"1"});
+                setFormValid({profile: true});
+                break;
+            case '2':
+                setVet(true);
+                setInput({profile: "2"});
+                setFormValid({profile: true});
+                break;
+            case '3':
+                setVet(false);
+                setInput({profile: "3"});
+                setFormValid({profile: true});
+                break;
+            default:
+                break;
         }
-        else{
-            setVet(false);
-            setInput({profile:"1"});
-        }
- 
+        
+       
     }
 
 
     return (        
         <Form className="register-form"  onFinish={register} onChange={changeForm}>            
-            <Form.Item>
-                <Input prefix={<MailOutlined className="site-form-item-icon" />} type="email" name="email" value={input.email} placeholder="Correo electronico" className="register-form__input"/>
+            <Form.Item rules={[{required: true, message: 'Ingrese su email'}]}>
+                <Input prefix={<MailOutlined className="site-form-item-icon" />} type="email" name="email" onChange={inputValidation} value={input.email} placeholder="Correo electronico" className="register-form__input"/>
             </Form.Item>
             <Form.Item>
-                <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" name="password" value={input.password} placeholder="Contrasenia" className="register-form__input"/>
+                <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" name="password" onChange={inputValidation} value={input.password} placeholder="Contrasenia" className="register-form__input"/>
             </Form.Item>
             <Form.Item>
-                <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" name="repeatPassword" value={input.repeatPassword} placeholder="Repetir Contrasenia" className="register-form__input"/>
+                <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" name="repeatPassword" onChange={inputValidation} value={input.repeatPassword} placeholder="Repetir Contrasenia" className="register-form__input"/>
             </Form.Item>
             <Form.Item>
-                <Input prefix={<UserOutlined className="site-form-item-icon" />} type="text" name="name" value={input.name} placeholder="Nombre" className="register-form__input"/>
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} type="text" name="name" onChange={inputValidation} value={input.name} placeholder="Nombre" className="register-form__input"/>
             </Form.Item>
             <Form.Item>
-                <Input prefix={<UserOutlined className="site-form-item-icon" />} type="text" name="lastName" value={input.lastName} placeholder="Apellido" className="register-form__input"/>
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} type="text" name="lastName" onChange={inputValidation} value={input.lastName} placeholder="Apellido" className="register-form__input"/>
             </Form.Item>
             <Form.Item>
                 <Input prefix={<UserOutlined className="site-form-item-icon" />} type="number" name="dni" value={input.dni} placeholder="D.N.I." className="register-form__input"/>
@@ -87,16 +178,16 @@ export default function RegisterForm(){
                 <Select name="profile" placeholder="Seleccione su perfil" className="register-form__select" value={input.profile} onChange={onProfileChange} allowClear > 
                     <Select.Option value="1" >Tutor</Select.Option>
                     <Select.Option value="2">Veterinario</Select.Option>
+                    <Select.Option value="3">Duenio Veterinaria</Select.Option>
                 </Select>
             </Form.Item>  
             {vet ? <div> <Form.Item id="matricula">
-                <Input prefix={<IdcardOutlined  className="site-form-item-icon" />} type="number" name="mp" value={input.mp} placeholder="Numero de Matricula" className="register-form__input"/>
+                <Input prefix={<IdcardOutlined  className="site-form-item-icon" />} type="number" name="mp" onChange={inputValidation} value={input.mp} placeholder="Numero de Matricula" className="register-form__input"/>
             </Form.Item></div>
-            : null}
-            
+            : null}          
             
             <Form.Item>
-                <Checkbox name="privacyPolicy" checked={input.privacyPolicy}>
+                <Checkbox name="privacyPolicy" onChange={inputValidation} checked={input.privacyPolicy}>
                     He leido y acepto la politica de privacidad
                 </Checkbox>
             </Form.Item>
@@ -106,8 +197,7 @@ export default function RegisterForm(){
                 </Button>
             </Form.Item>
         </Form>
-    );
-   
+    );  
     
         
 };
---
+++ src/utils/formValidation.js
@@ -0,0 +1,55 @@
+export function minLengthValidation(inputData, minLength) {
+    const {value}=inputData;
+
+    removeClassErrorSuccess(inputData);
+
+    if(value.length >= minLength){
+        inputData.classList.add("success");
+        return true;
+    } else{
+        inputData.classList.add("error");
+        return false;
+    }
+
+}
+
+export function emailValidation(inputData){
+    const emailValid = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    const {value}= inputData;
+
+    removeClassErrorSuccess(inputData);
+
+    const resultValidation = emailValid.test(value);
+    if(resultValidation){
+        inputData.classList.add("success");
+        return true;
+    }else{
+        inputData.classList.add("error");
+        return false;
+    };
+}
+//no haria falta hacer la comprobacion ya que el framework de antd hace automaticamente esto, 
+//pero solo para comprobaciones de numeros, excluye la comprobacion de numeros enteros
+// export function numberValidation(inputData){
+//     console.log(inputData);
+//     const numberValid = /[0-9]+$/;
+//     const {value} = inputData;
+
+//     removeClassErrorSuccess(inputData);
+
+//     const resultValidation = numberValid.test(value);
+
+//     if(resultValidation){
+//         inputData.classList.add("success");
+//         return true;
+//     }else{
+//         inputData.classList.add("error");
+//         return false;
+//     };
+// }
+
+function removeClassErrorSuccess(inputData){
+    inputData.classList.remove("success");
+    inputData.classList.remove("error");
+
+}

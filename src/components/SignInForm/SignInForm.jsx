c303974a17b9550da0df910e825800e3ec1c4329
tdbardin <tbardin@jemersoft.com>
2022-08-24 22:28:23 -0300

    feat: alert when cant login

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -3,6 +3,8 @@ import { Button, Form, Input, Checkbox, Typography, Space } from 'antd';
 import { LockOutlined, MailOutlined} from "@ant-design/icons";
 import { auth } from '../../services/auth.service';
 import {emailValidation, minLengthValidation} from '../../utils/formValidation';
+import { Alert } from 'antd';
+import { Spin } from 'antd';
 
 import '../RegisterForm/RegisterForm.scss'
 
@@ -13,8 +15,9 @@ export default function SignInForm(props){
     const [formValid, setFormValid] = useState({
         email: false,
         password: false,
-        
     });
+    const [warningAlert, setWarningAlert] = useState(null);
+    const [isTryingToLogin, setIsLogging] = useState(false);
 
     const changeForm = e =>{
         console.log(e.target.name);
@@ -25,13 +28,23 @@ export default function SignInForm(props){
     };
 
     const tryToLogIn = e => {
+        setIsLogging(true);
         auth(input.email,input.password)
             .then(res => {
-                sessionStorage.setItem('token',res.data.success);
-                props.logIn();
+                if(res.data.success){
+                    sessionStorage.setItem('token',res.data.success);
+                    props.logIn();
+                }
+                if(res.data.error){
+                    setWarningAlert(res.data.error);
+                    setInput({});
+                }
+                
+                setIsLogging(false);
             })
             .catch(e => {
-                console.error(e)
+                setWarningAlert("Servidor no accesible");
+                setIsLogging(false);
             });
     };
 
@@ -51,33 +64,59 @@ export default function SignInForm(props){
         };
     }
 
+    function AlertMessage() {
+        return warningAlert ? <Form.Item> <Alert message={warningAlert} type="error" showIcon /> </Form.Item> : null;
+    }
+
+    function LogginMessage() {
+        return isTryingToLogin ? <Spin size="middle"/> : <span>Ingresar</span>;
+    }
+
   return (
-    <Form className="register-form"  onFinish={tryToLogIn} onChange={changeForm}>            
+    <Form className="register-form"  onFinish={tryToLogIn} onChange={changeForm}>
+
             <Form.Item>
-                <Input prefix={<MailOutlined className="site-form-item-icon" />} type="email" name="email" placeholder="Correo electrónico" className="register-form__input" onChange={inputValidation}/>
+                <Input  prefix={<MailOutlined className="site-form-item-icon" />} 
+                        type="email" 
+                        name="email" 
+                        placeholder="Correo electrónico" 
+                        className="register-form__input" 
+                        onChange={inputValidation} 
+                        value={input.email}/>
             </Form.Item>
+
             <Form.Item>
-                <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" name="password" placeholder="Contraseña" className="register-form__input" onChange={inputValidation}/>
+                <Input  prefix={<LockOutlined className="site-form-item-icon" />} 
+                        type="password" 
+                        name="password" 
+                        placeholder="Contraseña" 
+                        className="register-form__input" 
+                        onChange={inputValidation}
+                        value={input.password}/>
             </Form.Item>
+
             <Form.Item>
                 <Checkbox name="session">
                     Mantener iniciada la sesión 
-
                 </Checkbox>
-            </Form.Item>            
-        
+            </Form.Item>
+
+            <AlertMessage></AlertMessage>
+            
             <Form.Item>
                 <Link to={'/menu'}>
                     <Button htmlType="submit" className="register-form__button"> 
-                        Ingresar
+                        <LogginMessage></LogginMessage>
                     </Button>
                 </Link>
             </Form.Item>
+        
             <Form.Item>
                 <Space align='center'>
                     <Link className="register-form__link">¿Recuperar contraseña?</Link>
                 </Space>
             </Form.Item>
+
         </Form>
   );
 };

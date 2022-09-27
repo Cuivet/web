222c5665f80e81b9e4c9e39a1afa5ab828603148
FlorenciaSantesso <florsantesso@gmail.com>
2022-09-27 00:51:15 -0300

    feat: update MenuCard

--- src/pages/MenuWeb/MenuWeb.jsx
+++ src/pages/MenuWeb/MenuWeb.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Row, Col, Typography, Divider } from "antd";
 import CardMenu from "../../components/CardMenu";
 import clipboard from "../../assets/img/jpg/clipboard.jpg";
+import huella from "../../assets/img/jpg/Huella1.jpg";
+import atencion from "../../assets/img/jpg/AtencionClinica.jpg";
+import calendario from "../../assets/img/jpg/calendario.jpg";
+import valorar from "../../assets/img/jpg/valorar.jpg";
+import clinica from "../../assets/img/jpg/clinica.jpg";
 import '../Settings/UserSettings/UserSettings.scss';
 
 const {Title} = Typography;
@@ -11,8 +16,8 @@ export default function MenuWeb(){
     const profile = JSON.parse(sessionStorage.getItem('profile')) ;
     // console.log(profile);
     
-    const vet = ['Consulta','Calendario','Ficha Clinica'];
-    const tutor = ['Mascotas','Calendario','Valorar','Historial Clinico'];
+    const vet = ['Atención Clínica','Calendario','Ficha Clínica'];
+    const tutor = ['Mis Mascotas','Calendario','Valorar Veterinarios','Historial Clínico'];
     const vetOwner = ['Veterinaria','Calendario'];
     // const test = "Hola";
 
@@ -34,24 +39,55 @@ export default function MenuWeb(){
     }
 
     //completar con datos traidos de la base
-    const modules = {
-        text:['Consulta', 'Mapa', 'Mascotas','Calendario', 'Valoracion', 'Historial'],
-        description:['Este es un modulo del sistema'],
+    const modulesVet = {
+        text:['Atención Clínica','Calendario','Ficha Clínica',],
+        description:['Iniciar Atención Clínica a la Mascota','Visualizá próximos eventos','Ver esta, se repite para mi',],
         disabled:[false,true],
-        img:[clipboard],
+        img:[atencion,calendario,clipboard,],
+    }
+
+    const modulesTut = {
+        text:['Mis Mascotas','Calendario','Valorar Veterinarios','Historial Clínico',],
+        description:['Visualizá tus Mascotas registradas','Visualizá próximos eventos','Registrá una reseña a tus Veterinarios asociados','Visualizá el Historial Clínico de tus Mascotas',],
+        disabled:[false,true],
+        img:[huella,calendario,valorar,atencion],
+    }
+
+    const modulesVetOw = {
+        text:['Veterinaria','Calendario',],
+        description:['Visualizá los datos de tu/s Veterinaria/s','Visualizá próximos eventos',],
+        disabled:[false,true],
+        img:[clinica,calendario,],
     }
 
     //fijo 4 modulos por fila 
     function Mod(){
         const mod = [];
         for(let i = 0; i < cantMod()[0].length; i++ ){
-            console.log(modules.text[i]);
-            mod.push(            
-                <Col xs={{ span: 24}} lg={{ span: 6 }}>
-                    <CardMenu key={i} text={cantMod()[0][i]} description={modules.description[0]} disabled={modules.disabled[0]} img={modules.img[0]} ></CardMenu>
-                </Col>
-            );      
-        
+            if(profile.veterinary){
+                console.log(modulesVet.text[i]);
+                mod.push(            
+                    <Col xs={{ span: 24}} lg={{ span: 6 }}>
+                        <CardMenu key={i} text={cantMod()[0][i]} description={modulesVet.description[i]} disabled={modulesVet.disabled[0]} img={modulesVet.img[i]} ></CardMenu>
+                    </Col>
+                ); 
+            }
+            if(profile.tutor){
+                console.log(modulesTut.text[i]);
+                mod.push(            
+                    <Col xs={{ span: 24}} lg={{ span: 6 }}>
+                        <CardMenu key={i} text={cantMod()[0][i]} description={modulesTut.description[i]} disabled={modulesTut.disabled[0]} img={modulesTut.img[i]} ></CardMenu>
+                    </Col>
+                ); 
+            }
+            if(profile.vetOwner){
+                console.log(modulesVetOw.text[i]);
+                mod.push(            
+                    <Col xs={{ span: 24}} lg={{ span: 6 }}>
+                        <CardMenu key={i} text={cantMod()[0][i]} description={modulesVetOw.description[i]} disabled={modulesVetOw.disabled[0]} img={modulesVetOw.img[i]} ></CardMenu>
+                    </Col>
+                ); 
+            }
         };               
         return mod;
     };

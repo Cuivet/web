3e8b61b2f68ef458432f690881a1c9b696fac1be
martina.barella <martinabarella98@gmail.com>
2022-09-26 23:51:35 -0300

    feat: regent association with vet

--- src/pages/VeterinariesManagement/VeterinariesManagement.jsx
+++ src/pages/VeterinariesManagement/VeterinariesManagement.jsx
@@ -10,10 +10,8 @@ const { Title } = Typography;
 export default function VeterinariesManagement(){
     const profile = JSON.parse(sessionStorage.getItem('profile'));
     const [isInit, setIsInit] = useState(false);
-    const [isModalVetOwner, setisModalVetOwner] = useState(false);
-    const [isModalRegent, setIsModalRegent]= useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [generatedCode, setGeneratedCode] = useState(false);
-    const [isRegent, setIsRegent] = useState(false);
     const [vetOptions, setVetOptions] = useState(null);
     const [selectedVetId, setSelectedVetId] = useState([]);
     const [mp, setMP] = useState(null);
@@ -77,11 +75,6 @@ export default function VeterinariesManagement(){
     const refreshSelectedVets = (value) => {
         setSelectedVetId(value);
     };
-    useEffect(() =>{
-        if(profile.veterinary != null){
-            setIsRegent(true);
-        }
-    },[profile.veterinary]);
 
     const regent=['Si', 'No'];
 
@@ -109,7 +102,19 @@ export default function VeterinariesManagement(){
             dataIndex: 'phone',
             //sorter: (a, b) => a.phone.age - b.phone.age,
             responsive: ['sm']
-        },        
+        },
+        {
+            title: 'Clínica Veterinaria',
+            dataIndex: 'vet',
+            sorter: (a, b) => a.vet.length - b.vet.length,
+            responsive: ['md']
+        },
+        {
+            title: 'Regente',
+            dataIndex: 'regent',
+            sorter: (a, b) => a.regent - b.regent,
+            responsive: ['md']
+        },
         {
             title: 'Dirección',
             dataIndex: 'address',
@@ -123,82 +128,14 @@ export default function VeterinariesManagement(){
             
         }
     ];
-      
-    if(!isRegent){
-        columns.splice(4,0, {
-            title: 'Regente',
-            dataIndex: 'regent',
-            sorter: (a, b) => a.regent - b.regent,
-            responsive: ['md']
-        },
-        {
-            title: 'Clínica Veterinaria',
-            dataIndex: 'vet',
-            sorter: (a, b) => a.vet.length - b.vet.length,
-            responsive: ['md']
-        });
-    };
-    
-
-    const data = [
-        {
-            key: '1',
-            mp: '3029',
-            name: 'Eugenia',
-            lastName: 'Frattin',
-            phone: 3513026921,
-            vet: 'NeoZoo',
-            regent: regent[0],
-            //en la condicion deberia ir lo que trae el back
-            actions: (regent[0] === 'Si') ? (<Tooltip placement='top' title="Desvincular"><Button type='link' className='appTableButton' icon={<SyncDisabledOutlinedIcon></SyncDisabledOutlinedIcon>}></Button></Tooltip>) : null,            
-            address: 'Rodríguez del Busto 4086',
-        },
-        {
-            key: '2',
-            mp: '2041',
-            name: 'Jorge Ignacio',
-            lastName: 'Barbará',
-            phone: 3513020874,
-            vet: 'Pecos',
-            regent: regent[1],
-            address: 'Arturo M. Bas 345',
-            actions: (regent[1] === 'Si') ? (<Tooltip placement='top' title="Desvincular"><Button type='link' className='appTableButton' icon={<SyncDisabledOutlinedIcon></SyncDisabledOutlinedIcon>}></Button></Tooltip>) : null,
-        },
-        {
-            key: '3',
-            mp: '1098',
-            name: 'Juan',
-            lastName: 'Barella',
-            phone: 351739744,
-            vet: 'NeoZoo',
-            regent: regent[0],
-            actions: (regent[0] === 'Si') ? (<Tooltip placement='top' title="Desvincular"><Button type='link' className='appTableButton' icon={<SyncDisabledOutlinedIcon></SyncDisabledOutlinedIcon>}></Button></Tooltip>) : null,
-            address: 'Av. Rafael Núñez 4126',
-        },
-        {
-            key: '4',
-            mp: '2270',
-            name: 'Martina',
-            lastName: 'Aresu',
-            phone: 3515521098,
-            vet: 'Alem',
-            regent: regent[1],
-            actions: (regent[1] === 'Si') ? (<Tooltip placement='top' title="Desvincular"><Button type='link' className='appTableButton' icon={<SyncDisabledOutlinedIcon></SyncDisabledOutlinedIcon>}></Button></Tooltip>) : null,
-            address: 'Bv. Los Granaderos 2075',
-        }
-
-    ];
 
     const onChange = (pagination, filters, sorter, extra) => {
         console.log('params', pagination, filters, sorter, extra);
     };
 
-    const showModalOwner = () => {
-        setisModalVetOwner(true);
+    const showModal = () => {
+        setIsModalOpen(true);
     };
-    const showModalRegent = () => {
-        setIsModalRegent(true);
-    }
     
     const generateCode = () => {
         setIsLoading(true);
@@ -211,9 +148,8 @@ export default function VeterinariesManagement(){
     }; 
 
     const hideModal = () => {
-        setisModalVetOwner(false);
+        setIsModalOpen(false);
         setGeneratedCode(false);
-        setIsModalRegent(false);
     };
 
     const refreshMP = e =>{
@@ -223,31 +159,16 @@ export default function VeterinariesManagement(){
     return (
         <>   
             <Row align="middle">
-                {
-                    isRegent ?
-                    <>
-                        <Col xs={{span:24}} md={{span:23}}>
-                            <Title className='appTitle'>Gestion de Veterinarios</Title>
-                        </Col>
-                        <Col xs={{span:24}} md={{span:1}}>
-                            <Tooltip title="Asociar Co-Veterinarios" placement='right'>
-                                <Button type='link' className="appButton" size='large' icon={<NodeIndexOutlined/>} onClick={showModalRegent}/>
-                            </Tooltip>
-                        </Col>
-                    </>
-                    :
-                    <>
-                        <Col xs={{span:24}} md={{span:23}}>
-                            <Title className='appTitle'>Gestion de Veterinarios en Clinicas</Title>
-                        </Col>
-                        <Col xs={{span:24}} md={{span:1}}>
-                            <Tooltip title="Asociar Veterinario Regente" placement='right'>
-                                <Button type='link' className="appButton" size='large' icon={<NodeIndexOutlined/>} onClick={showModalOwner}/>
-                            </Tooltip>
-                        </Col>
-                    </>
-                }    
+                <Col xs={{span:24}} md={{span:23}}>
+                    <Title className='appTitle'>Gestión de Veterinarios en Mis Clínicas</Title>
+                </Col>
+                <Col xs={{span:24}} md={{span:1}}>
+                    <Tooltip title="Asociar Veterinario Regente" placement='right'>
+                        <Button type='link' className="appButton" size='large' icon={<NodeIndexOutlined/>} onClick={showModal}/>
+                    </Tooltip>
+                </Col>
             </Row>
+
             <Divider orientation="left">Filtros</Divider>
             
             <Row gutter={[16, 16]}>
@@ -267,27 +188,15 @@ export default function VeterinariesManagement(){
                         <Option value="Pecos">Pecos</Option>
                     </Select>
                 </Col>
-                {
-                    isRegent ?
-                    <>
-                        <Col className="gutter-row" xs={{span:12}} md={{span:8}}>
-                            <Input placeholder='Telefono' />
-                        </Col>
-                    </>
-                    :
-                    <>
-                        <Col className="gutter-row" xs={{span:12}} md={{span:4}}>
-                            <Select placeholder="Regente" showSearch className="select-before full-width">
-                                <Option value="Si">Si</Option>
-                                <Option value="No">No</Option>
-                            </Select>
-                        </Col>
-                        <Col className="gutter-row" xs={{span:12}} md={{span:4}}>
-                            <Input placeholder='Telefono' />
-                        </Col>
-                    </>
-                }
-                
+                <Col className="gutter-row" xs={{span:12}} md={{span:4}}>
+                    <Select placeholder="Regente" showSearch className="select-before full-width">
+                        <Option value="Si">Si</Option>
+                        <Option value="No">No</Option>
+                    </Select>
+                </Col>
+                <Col className="gutter-row" xs={{span:12}} md={{span:4}}>
+                    <Input placeholder='Telefono' />
+                </Col>
                 <Col className="gutter-row" xs={{span:12}} md={{span:8}}>
                     <Input placeholder='Dirección' />
                 </Col>
@@ -295,54 +204,9 @@ export default function VeterinariesManagement(){
             
             <Divider orientation="left"></Divider>
             <Table columns={columns} dataSource={data} onChange={onChange} />
-            <Modal  title="Generar código de asociacion con Veterinario Regente"
-                    visible={isModalVetOwner}
-                    onCancel={hideModal}
-                    footer={[
-                        <Button type="default" onClick={hideModal} className="register-form__button-cancel-modal" > 
-                            Cancelar
-                        </Button>,
-                        <>
-                            {
-                            generatedCode ? 
-                            <Button htmlType="submit" type="primary" onClick={hideModal} className="register-form_button-ok-modal" > 
-                                Aceptar
-                            </Button>
-                            :
-                            <Button htmlType="submit" type="primary" onClick={generateCode} className="register-form_button-ok-modal" > 
-                                Generar
-                            </Button>
-                            }
-                        </>
-                    ]}>
-                {
-                generatedCode ?
-                <><Row>
-                        <Col span={24}>
-                            <Typography.Title level={4}>
-                                El código generado es:
-                            </Typography.Title>
-                        </Col>                        
-                    </Row>
-                    <Row>
-                        <Col span={24}>
-                            <Typography.Title style={{display:'flex', justifyContent:'center'}} copyable={{tooltips:['click para copiar', 'codigo copiado']}}>20202461</Typography.Title>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
 
-                        </Col>
-                    </Row>El mismo expirará en 10 minutos</>
-                :
-                <>
-                    <div>Ingrese la matricula del Veterinario a asociar</div>
-                    <Input type="number" name="mp" placeholder="M.P. Veterinario"/>
-                </>
-                }
-            </Modal>
-            <Modal  title="Generar código de asociacion con el Co-Veterinario"
-                    visible={isModalRegent}
+            <Modal  title="Generar código de asociación con Veterinario Regente"
+                    visible={isModalOpen}
                     onCancel={hideModal}
                     footer={[
                         <Button type="default" onClick={hideModal} className="register-form__button-cancel-modal" > 
@@ -419,4 +283,3 @@ export default function VeterinariesManagement(){
         </>
     );
 };
-

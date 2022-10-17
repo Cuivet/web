bdaadb52860d08e592207cd0e6fdf9cc3b67f2f4
FlorenciaSantesso <florsantesso@gmail.com>
2022-10-17 20:20:40 -0300

    feat: update menu web

--- src/components/CardMenu/CardMenu.jsx
+++ src/components/CardMenu/CardMenu.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Card } from "antd";
 import Meta from "antd/lib/card/Meta";
+import { Link } from "react-router-dom";
 
 import './CardMenu.scss';
 
 
 export default function CardMenu(props){
-    const {img, text, description, disabled} = props;
+    const {img, text, description, disabled, route} = props;
     return (
-        
-        <Card hoverable cover={<img alt='required field' src={img} ></img>} loading={disabled} className='card-menu'>
-            <Meta title={text} description={description} className='card-menu__meta'></Meta>
-        </Card>
+        <Link to={route} style={{cursor: 'pointer'}}>
+            <Card hoverable
+                cover={<img alt='required field' src={img}></img>}
+                loading={disabled}
+                className='card-menu'>
+                <Meta title={text} description={description} className='card-menu__meta'></Meta>
+            </Card>
+        </Link>
+
     )
 }
--- src/pages/MenuWeb/MenuWeb.jsx
+++ src/pages/MenuWeb/MenuWeb.jsx
@@ -1,98 +1,90 @@
 import React from "react";
 import { Row, Col, Typography, Divider } from "antd";
 import CardMenu from "../../components/CardMenu";
-import clipboard from "../../assets/img/jpg/clipboard.jpg";
 import huella from "../../assets/img/jpg/Huella1.jpg";
 import atencion from "../../assets/img/jpg/AtencionClinica.jpg";
 import calendario from "../../assets/img/jpg/calendario.jpg";
 import valorar from "../../assets/img/jpg/valorar.jpg";
 import clinica from "../../assets/img/jpg/clinica.jpg";
 import '../Settings/UserSettings/UserSettings.scss';
-import {Link} from "react-router-dom";
 
 const {Title} = Typography;
 
 export default function MenuWeb(){
 
     const profile = JSON.parse(sessionStorage.getItem('profile')) ;
-    // console.log(profile);
-    
-    const vet = ['Atención Clínica','Calendario','Ficha Clínica'];
-    const tutor = ['Mis Mascotas','Calendario','Valorar Veterinarios','Historial Clínico'];
-    const vetOwner = ['Veterinaria','Calendario'];
-    // const test = "Hola";
 
-    function cantMod(){
-        //definir modulos comunes en todos los perfiles
-        let mod_text = [];
-        // let flag = false;
-        if(profile.tutor != null){
-            mod_text.push(tutor);
-        }
-        if(profile.veterinary != null){
-            mod_text.push(vet);
-        }
-        if(profile.vetOwner!= null){
-            mod_text.push(vetOwner);
-        }
-        // console.log(mod_text); 
-        return mod_text;
-    }
-
-    //completar con datos traidos de la base
-    const modulesVet = {
-        text:['Atención Clínica','Calendario','Ficha Clínica',],
-        description:['Iniciar Atención Clínica a la Mascota','Visualizá próximos eventos','Ver esta, se repite para mi',],
+    const veterinary = {
+        module:['Consulta Médica','Calendario','Mascotas asociadas',],
+        description:['Iniciar Consulta para la Mascota','Visualizá próximos eventos','Visualizá las mascotas asociadas',],
         disabled:[false,true],
-        img:[atencion,calendario,clipboard,],
-        routes:['/admin','/admin','/admin'],
-    }
+        img:[atencion,calendario,huella,],
+        routes:['/consultation','/calendar','/pets-management']
+    };
 
-    const modulesTut = {
-        text:['Mis Mascotas','Calendario','Valorar Veterinarios','Historial Clínico',],
+    const tutor = {
+        module:['Mis Mascotas','Calendario','Valorar Veterinarios','Historial Clínico',],
         description:['Visualizá tus Mascotas registradas','Visualizá próximos eventos','Registrá una reseña a tus Veterinarios asociados','Visualizá el Historial Clínico de tus Mascotas',],
         disabled:[false,true],
         img:[huella,calendario,valorar,atencion],
-        routes:['/admin','/admin','/admin'],
-    }
+        routes:['/pets','/calendar','/admin','/admin']
+    };
 
-    const modulesVetOw = {
-        text:['Veterinaria','Calendario',],
+    const vetOwner = {
+        module:['Veterinaria','Calendario',],
         description:['Visualizá los datos de tu/s Veterinaria/s','Visualizá próximos eventos',],
         disabled:[false,true],
         img:[clinica,calendario,],
+        routes:['/admin','/calendar']
+    };
+
+    function cantMod(){
+        //definir modulos comunes en todos los perfiles, calendar por ejemplo
+        let cards = {
+            module: [],
+            description:[],
+            disabled: [false,true],
+            img: [],
+            routes:[]
+        };
+        // let flag = false;
+        if(profile.tutor != null){
+            cards.module.push(tutor.module);
+            cards.description.push(tutor.description);
+            cards.img.push(tutor.img);
+            cards.routes.push(tutor.routes);
+        }
+        if(profile.veterinary != null){
+            cards.module.push(veterinary.module);
+            cards.description.push(veterinary.description);
+            cards.img.push(veterinary.img);
+            cards.routes.push(veterinary.routes);
+        }
+        if(profile.vetOwner!= null){
+            cards.module.push(vetOwner.module);
+            cards.description.push(vetOwner.description);
+            cards.img.push(vetOwner.img);
+            cards.routes.push(vetOwner.routes);
+        }
+        return cards;
     }
 
     //fijo 4 modulos por fila 
     function Mod(){
-        const mod = [];
-        for(let i = 0; i < cantMod()[0].length; i++ ){
-            if(profile.veterinary){
-                console.log(modulesVet.text[i]);
-                mod.push(            
-                    <Col xs={{ span: 24}} lg={{ span: 6 }}>
-                        <CardMenu key={i} text={cantMod()[0][i]} description={modulesVet.description[i]} disabled={modulesVet.disabled[0]} img={modulesVet.img[i]}><Link to={modulesVet.routes[i]}>Hace click</Link></CardMenu>
-                    </Col>
-                ); 
-            }
-            if(profile.tutor){
-                console.log(modulesTut.text[i]);
-                mod.push(            
-                    <Col xs={{ span: 24}} lg={{ span: 6 }}>
-                        <CardMenu key={i} text={cantMod()[0][i]} description={modulesTut.description[i]} disabled={modulesTut.disabled[0]} img={modulesTut.img[i]}></CardMenu>
-                    </Col>
-                ); 
-            }
-            if(profile.vetOwner){
-                console.log(modulesVetOw.text[i]);
-                mod.push(            
-                    <Col xs={{ span: 24}} lg={{ span: 6 }}>
-                        <CardMenu key={i} text={cantMod()[0][i]} description={modulesVetOw.description[i]} disabled={modulesVetOw.disabled[0]} img={modulesVetOw.img[i]} ></CardMenu>
-                    </Col>
-                ); 
-            }
+        const renderModules = [];
+        for(let i = 0; i < cantMod().module[0].length; i++ ){
+            renderModules.push(
+                <Col xs={{span: 24 }} lg={{span: 6 }}>
+                    <CardMenu kei={i}
+                    route={cantMod().routes[0][i]}
+                    text={cantMod().module[0][i]}
+                    description={cantMod().description[0][i]}
+                    disabled={cantMod().disabled[0]}
+                    img={cantMod().img[0][i]}></CardMenu>
+                </Col>
+            )
         };               
-        return mod;
+        return renderModules;
     };
 
     return (

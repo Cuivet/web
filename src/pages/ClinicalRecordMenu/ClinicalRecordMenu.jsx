9af1bdeb83f211229c8ccb3e2a11a0e0a603bd94
tomas <tbardin@jemersoft.com>
2022-11-21 14:30:35 -0300

    feat: clinical record menu bugs and refactor

--- src/pages/ClinicalRecordMenu/ClinicalRecordMenu.jsx
+++ src/pages/ClinicalRecordMenu/ClinicalRecordMenu.jsx
@@ -10,6 +10,7 @@ import {
   Spin,
   Select,
   Alert,
+  Card
 } from "antd";
 import { getTutorDataByDni } from "../../services/tutor.service";
 import AvatarSearch from "../../components/AvatarSearch";
@@ -25,7 +26,7 @@ export default function ClinicalRecordMenu() {
   const [isSearchingTutorData, setIsSearchingTutorData] = useState(false);
   const [searchedTutorData, setSearchedTutorData] = useState(null);
   const [petOptions, setPetOptions] = useState(null);
-  const [selectedPetId, setSelectedPetId] = useState([]);
+  const [selectedPetId, setSelectedPetId] = useState(null);
 
   const refreshDni = (e) => {
     setSearchedTutorData(null);
@@ -76,129 +77,124 @@ export default function ClinicalRecordMenu() {
         </Col>
       </Row>
 
-      <Divider orientation="center">Nueva ficha clínica</Divider>
-
-      <>
-        {/* <Divider orientation="left" plain> Ingrese DNI del tutor de la mascota </Divider> */}
-        <Row>
-          <Col
-            xs={{ span: 24 }}
-            md={{ span: 6, offset: 9 }}
-            style={{ marginBottom: "1%" }}
-          >
-            <Typography.Text>
-              Ingrese DNI del tutor de la mascota:
-            </Typography.Text>
-          </Col>
-          <Col xs={{ span: 24 }} md={{ span: 4, offset: 9 }}>
-            <Input
-              type="number"
-              name="phone"
-              placeholder="DNI del tutor a asociar"
-              onChange={refreshDni}
-              allowClear
-            />
-          </Col>
-          <Col xs={{ span: 24 }} md={{ span: 2, offset: 0 }}>
-            <Button
-              htmlType="submit"
-              type="primary"
-              style={{ width: "100%" }}
-              onClick={searchTutorData}
-              className="register-form_button-ok-modal"
-            >
-              Buscar tutor
-            </Button>
-          </Col>
-        </Row>
-        <br></br>
-        <Row span={5}>
-          {searchedTutorData ? (
-            <Col xs={{ span: 24 }} md={{ span: 6, offset: 9 }}>
-              <AvatarSearch
-                name={searchedTutorData.person.name}
-                lastName={searchedTutorData.person.lastName}
-              ></AvatarSearch>
-            </Col>
-          ) : isSearchingTutorData ? (
-            <>
-              <Spin />
-              Buscando...
-            </>
-          ) : (
-            <Col xs={{ span: 24 }} md={{ span: 6, offset: 9 }}>
-              <Alert
-                message="Aun no has seleccionado ningún tutor"
-                type="warning"
-              />
-            </Col>
-          )}
-        </Row>
-        <Row>
-          <Col
-            xs={{ span: 24 }}
-            md={{ span: 6, offset: 9 }}
-            style={{ marginBottom: "1%", marginTop: "3%" }}
-          >
-            <Typography.Text>Seleccione una mascota:</Typography.Text>
-          </Col>
-          {searchedTutorData ? (
-            <>
-              <Col xs={{ span: 24 }} md={{ span: 6, offset: 9 }}>
-                <Select
-                  allowClear
-                  style={{ width: "100%" }}
-                  placeholder="Seleccione la mascota a atender"
-                  onChange={refreshSelectedPet}
-                >
-                  {petOptions}
-                </Select>
-              </Col>
-              <Col span={24}>
-                <br></br>
-                {selectedPetId ? (
-                  <Col xs={{ span: 24 }} md={{ span: 2, offset: 11 }}>
-                    <Button
-                      htmlType="submit"
-                      type="primary"
-                      className="register-form_button-ok-modal"
-                      onClick={createClinicalRecord}
-                      shape="round"
-                    >
-                      Crear
-                    </Button>
-                  </Col>
-                ) : (
-                  <>
-                    <Row>
-                      <Col xs={{ span: 24 }} md={{ span: 6, offset: 9 }}>
+      <Divider orientation="left">Nueva ficha clínica</Divider>
+      
+      <Row>
+        <Col sm={{ span: 24 }} md={{ span: 12 }} lg={{ span: 9 }}>
+          <Card title="Datos para la nueva ficha" hoverable
+            actions={[
+              <Col>
+                {
+                  selectedPetId ?
+                    <Col xs={{ span: 24 }}>
+                      <Button
+                        htmlType="submit"
+                        type="primary"
+                        className="register-form_button-ok-modal"
+                        onClick={createClinicalRecord}
+                        shape="round"
+                      >
+                        Crear
+                      </Button>
+                    </Col>
+                  :
+                    <div style={{ marginLeft: "2%", marginRight: "2%" }}>
                         <Alert
-                          message={"Información "}
-                          description="Debes seleccionar una mascota para poder crear la ficha clínica"
+                          message={searchedTutorData ? "Debes seleccionar una mascota para poder crear la ficha clínica" : "Debes seleccionar un tutor para poder crear la ficha clínica"}
                           type="info"
                           icon={<SettingTwoTone twoToneColor="#523c89" spin />}
                           showIcon
                         ></Alert>
-                      </Col>
-                    </Row>
-                  </>
-                )}
+                    </div>
+                }
               </Col>
-            </>
-          ) : (
-            <Col xs={{ span: 24 }} md={{ span: 6, offset: 9 }}>
-              <Alert
-                message="Información "
-                description="Debes buscar un tutor para poder avanzar con la creación de la
-                ficha clínica"
-                type="info"
-                icon={<SettingTwoTone twoToneColor="#523c89" spin />}
-                showIcon
-              ></Alert>
-            </Col>
-          )}
-        </Row>
-      </>
+            ]}>
+            <Row>
+              <Col
+                style={{ marginBottom: "1%" }}
+              >
+                <Typography.Text>
+                  Ingrese DNI del tutor de la mascota:
+                </Typography.Text>
+              </Col>
+              <Col xs={{ span: 24 }}>
+                <Input
+                  type="number"
+                  name="phone"
+                  placeholder="DNI del tutor a asociar"
+                  onChange={refreshDni}
+                  allowClear
+                />
+              </Col>
+              <Col xs={{ span: 24 }}>
+                <Button
+                  htmlType="submit"
+                  type="primary"
+                  style={{ width: "100%" }}
+                  onClick={searchTutorData}
+                  className="register-form_button-ok-modal"
+                >
+                  Buscar tutor
+                </Button>
+              </Col>
+            </Row>
+            <br></br>
+            <Row span={5}>
+              {
+              searchedTutorData 
+              ?
+                <Col xs={{ span: 24 }}>
+                  <AvatarSearch
+                    name={searchedTutorData.person.name}
+                    lastName={searchedTutorData.person.lastName}
+                  ></AvatarSearch>
+                </Col>
+              : 
+                isSearchingTutorData 
+                ?
+                  <>
+                    <Spin />
+                    Buscando...
+                  </>
+                :
+                  <Col xs={{ span: 24 }}>
+                    <Alert
+                      message="Aun no has seleccionado ningún tutor"
+                      type="warning"
+                    />
+                  </Col>
+              }
+            </Row>
+            <Row>
+              {
+                searchedTutorData ?
+                  <>
+                    <Col
+                      xs={{ span: 24 }}
+                      style={{ marginBottom: "1%", marginTop: "3%" }}
+                    >
+                      <Typography.Text>Seleccione una mascota:</Typography.Text>
+                    </Col>
+                    <Col xs={{ span: 24 }}>
+                      <Select
+                        allowClear
+                        style={{ width: "100%" }}
+                        placeholder="Seleccione la mascota a atender"
+                        onChange={refreshSelectedPet}
+                      >
+                        {petOptions}
+                      </Select>
+                    </Col>
+                  </>
+                :
+                  <>
+                  </>
+              }
+            </Row>
+          </Card>
+        </Col>
+      </Row>
     </>
   );
+  
 }

e3e3716ef889337bea86fcc88f98a8f922970df2
Martina Barella <martinabarella98@gmail.com>
2024-12-16 11:44:45 -0300

    Fix: Species filter added to pets by tutor in reports

--- src/services/pet.service.js
+++ src/services/pet.service.js
@@ -1,107 +1,122 @@
 import { basePATH } from "./config";
 
-export function registerPet(data){
-    var axios = require('axios');
-    return axios({
-        method: "post",
-        url: `${basePATH}/pet/register`,
-        data: data,
-        timeout: 1000 * 5, // Wait for 5 seconds
-        headers: {
-            "Content-Type": "application/json",
-            Accept: 'application/json',
-            "token": sessionStorage.getItem('token'),
-        }
-        })
-        .then((response) => {
-            return response;
-        })
-        .catch((err) => {
-            return err;
-        });
+export function registerPet(data) {
+  var axios = require("axios");
+  return axios({
+    method: "post",
+    url: `${basePATH}/pet/register`,
+    data: data,
+    timeout: 1000 * 5, // Wait for 5 seconds
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      token: sessionStorage.getItem("token"),
+    },
+  })
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      return err;
+    });
 }
 
 export function getPetsByTutorId(tutorId) {
-    var axios = require('axios');
-    console.log(sessionStorage.getItem('token'));
-    return axios({
-        method: "get",
-        url: `${basePATH}/pet/allByTutorId/${tutorId}`,
-        timeout: 1000 * 5, // Wait for 5 seconds
-        headers: {
-            "Content-Type": "application/json",
-            Accept: 'application/json',
-            "token": sessionStorage.getItem('token'),
-        }
-        })
-        .then((response) => {
-            return response.data;
-        })
-        .catch((err) => {
-            return err;
-        });
-       
-};
+  var axios = require("axios");
+  return axios({
+    method: "get",
+    url: `${basePATH}/pet/allByTutorId/${tutorId}`,
+    timeout: 1000 * 5, // Wait for 5 seconds
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      token: sessionStorage.getItem("token"),
+    },
+  })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+}
 
 export function getPetsById(id) {
-    var axios = require('axios');
-    console.log(sessionStorage.getItem('token'));
-    return axios({
-        method: "get",
-        url: `${basePATH}/pet/allById/${id}`,
-        timeout: 1000 * 5, // Wait for 5 seconds
-        headers: {
-            "Content-Type": "application/json",
-            Accept: 'application/json',
-            "token": sessionStorage.getItem('token'),
-        }
-        })
-        .then((response) => {
-            return response.data;
-        })
-        .catch((err) => {
-            return err;
-        });
-       
-};
-
+  var axios = require("axios");
+  return axios({
+    method: "get",
+    url: `${basePATH}/pet/allById/${id}`,
+    timeout: 1000 * 5, // Wait for 5 seconds
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      token: sessionStorage.getItem("token"),
+    },
+  })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+}
 
 export function deletePet(petId) {
-    var axios = require('axios');
-    return axios({
-        method: "delete",
-        url: `${basePATH}/pet/${petId}`,
-        timeout: 1000 * 5, // Wait for 5 seconds
-        headers: {
-            "Content-Type": "application/json",
-            Accept: 'application/json',
-            "token": sessionStorage.getItem('token'),
-        }
-        })
-        .then((response) => {
-            return response.data;
-        })
-        .catch((err) => {
-            return err;
-        });
-};
+  var axios = require("axios");
+  return axios({
+    method: "delete",
+    url: `${basePATH}/pet/${petId}`,
+    timeout: 1000 * 5, // Wait for 5 seconds
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      token: sessionStorage.getItem("token"),
+    },
+  })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+}
 
 export function getPet(petId) {
-    var axios = require('axios');
-    return axios({
-        method: "get",
-        url: `${basePATH}/pet/${petId}`,
-        timeout: 1000 * 5, // Wait for 5 seconds
-        headers: {
-            "Content-Type": "application/json",
-            Accept: 'application/json',
-            "token": sessionStorage.getItem('token')
-        }
-        })
-        .then((response) => {
-            return response;
-        })
-        .catch((err) => {
-            return err;
-        });
-  }
+  var axios = require("axios");
+  return axios({
+    method: "get",
+    url: `${basePATH}/pet/${petId}`,
+    timeout: 1000 * 5, // Wait for 5 seconds
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      token: sessionStorage.getItem("token"),
+    },
+  })
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      return err;
+    });
+}
+
+export function getPetsDataByTutorId(tutorId) {
+  var axios = require("axios");
+  return axios({
+    method: "get",
+    url: `${basePATH}/pet/allDataByTutorId/${tutorId}`,
+    timeout: 1000 * 5, // Wait for 5 seconds
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      token: sessionStorage.getItem("token"),
+    },
+  })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+}
